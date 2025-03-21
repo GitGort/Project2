7b367737b5f0c595c0c225ff2559c83428c9954b
mem3910 <mem3910@g.rit.edu>
2025-03-21 10:28:40 -0400

    Update game.h

--- game.h
+++ game.h
@@ -12,19 +12,52 @@ class Item {
     string desc;
 
   public:
-    item(string n, string d) { name = n, desc = d;}
-
+    item(string n, string d) { name = n, desc = d}; //constructor
+    string getName(); //getter
+    string getDesc(); //getter
 
 };
 
 
 class Enemy {
   /*TODO: write your code*/
+
+  private:
+
+    string name;
+    int health;
+    int attackPower;
+
+  public:
+
+    Enemy(string n, int h, int a) {name = n, health = h, attackPower = a}; //constructor
+    string getName(); //getter
+    int getHealth(); //getter
+    int getAttackPower(); //getter
+    void setHealth(int h); //setter
+
 };
 
 
 class Place {
   /*TODO: write your code*/
+
+  private:
+
+    string desc;
+
+  public:
+
+    //vectors
+   // item(itemList)
+
+
+
+    //functions
+    Place(string d) {desc = d}; //constructor
+    string getDesc(); //getter
+    
+
 };
 
 
